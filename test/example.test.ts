0a63f3afa3631b9c76fcf5960641767e1dfa945b
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-12-06 22:35:04 +0300

    refactor: update snapshots and tests to reflect Avestic component changes

--- test/example.test.ts
+++ test/example.test.ts
@@ -2,29 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
 import { Avestic } from '../src'
 
-const slotText = 'Test content'
-
-describe('button', () => {
-  it('alert render', () => {
-    const wrapper = mount(Avestic, {
-      slots: {
-        default: slotText,
-      },
-    })
-
-    expect(wrapper.text()).toBe(slotText)
-  })
-
-  it('loading', () => {
-    const wrapper = mount(Avestic, {
-      props: {
-        loading: true,
-      },
-    })
-
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
+describe('avestic', () => {
   it('disabled', () => {
     const wrapper = mount(Avestic, {
       props: {
